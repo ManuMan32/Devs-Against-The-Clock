95582f54135b4632fb753557b37988775e34d804
Manu <141773224+ManuMan32@users.noreply.github.com>
2024-01-17 07:17:30 -0300

    Delete dist directory

--- dist/questions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const questions = [];
-function createQuestion(statement, answer, options) {
-    return { id: (questions.length + 1), statement, answer, options };
-}
-questions.push(createQuestion("Este es el enunciado de mi pregunta", "si", ["no", "tal vez"]));
-console.log(questions[0]);
-//# sourceMappingURL=questions.js.map
